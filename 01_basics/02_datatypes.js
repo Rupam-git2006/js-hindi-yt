4b4a79080789f099a5e1c35273caba8cae85f2ca
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-06 13:46:25 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code  as newer version
+//alert(2+2) /*In node JS alert is not defined ,its run only browsers because there are pre compiled source/*
+
+
+let name = "rupam"//string datatype
+let age = 18// number datatype
+let isPrime = false // boolian datatype
+let state = null
+console.log(typeof null);
+console.log(typeof false);
+console.log(typeof 1);
+console.log(typeof undefined);
+
+
+
+// number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone  vlaue
+//undefined =>
+
