0808fa5c7128b58c7f25f1286b2477a11d13dd97
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-06 18:12:25 +0000

    operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,23 @@
+let value= 3
+let neg = -value
+console.log(neg);
+
+
+let str1 = "Rupam"
+let str2 = " Samanta"
+let str3 =str1+str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + '2');
+console.log(1 + 1 + '2');
+console.log(1 + "1" + '2');
+console.log("1" + 1 + 2);
+
+
+console.log(+true);
+console.log(+false);
+console.log(-true);
+console.log(-false);
+
