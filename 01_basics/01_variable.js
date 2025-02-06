d58b394494679c5e0cc00a692fe81af4d48a25a3
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-06 05:56:42 +0000

    container change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 325317
+let accountEmail = "jfsfb@com"
+var accountCity = "Jaipur"
+accountPassword = "123"
+//In JS variable are allocated aujtomaticaly
+
+
+/*
+Prefer not to use var because of iisue in block scope and functional scope
+*/
+
+accountEmail="hdsg@com"
+accountCity="Barasat"
+//accountId=2
+console.log(accountId);
+console.table([accountId,accountCity,accountEmail,accountPassword])
