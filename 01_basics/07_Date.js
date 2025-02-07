6202f84d30d85c6a7b18da681d9c1f43e823e0be
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-07 17:34:34 +0000

    hi

--- 01_basics/07_Date.js
+++ 01_basics/07_Date.js
@@ -9,6 +9,8 @@ console.log(myDate.toLocaleDateString());
 console.log(myDate.toTimeString());
 console.log(myDate.toUTCString());
 
+let newDate = new Date()
+console.log(newDate.getMonth()+1);
 
 
 let myCreat = new Date(2025,0,23)
@@ -17,3 +19,9 @@ let myCreat2 = new Date(2024,1,23,5,2)
 console.log(myCreat2.toLocaleString());
 let myCreat3 = new Date("01-14-2024")
 console.log(myCreat3.toLocaleString());
+
+
+let myTime = Date.now()
+console.log(myTime);
+console.log(myCreat3.getTime());
+console.log(Math.floor(Date.now()/1000));
