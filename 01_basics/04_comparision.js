e22a33762aa3fc3a7f7f5800f364acc3352499ff
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-07 05:33:23 +0000

    datatype

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+console.log("2">"1");
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(undefined==0);
+
+//   ===   this is check data types 
+console.log("2"===2);
---
+++ 01_basics/Datatype_Summary.js
@@ -0,0 +1,18 @@
+//Primitive
+//7 types : String, Number,Boolean , null, undefined,symbol , Bigint
+
+const score = 100
+const sccorevalue = 100.3
+const isLoggedIn= false
+const outsideTemp = null
+
+
+const id = Symbol('123')
+
+const another= Symbol('123')
+console.log(another === id);
+
+
+
+//Reference (Non primitive)
+//Array , Object , Function
