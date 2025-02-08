0280a3129b1ddf410b1afdbff2ef2a4d953e581e
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-08 05:49:46 +0000

    hi

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,16 +1,29 @@
 //singleton
 //ovbject literals
 
-const mySym = Symbol(key1);
+const mySym = Symbol("key1");
 const Jsuser ={
     name : "hitesh",
     "full name" : "hitesh choudhary",
-    mySym : "myKey1",
+    [mySym] : "mykey1",
     age : 22,
+    email:"rhtirn@com",
     courseCount : 5,
     isLoggedin : true
 }
 console.log(Jsuser.name);
 console.log(Jsuser["name"]);
 console.log(Jsuser["full name"]);
-console.log(Jsuser.mySym);
+console.log(Jsuser[mySym]);
+
+Jsuser.email = "rupam@com";  //This is method of change objects
+console.log(Jsuser.email);
+Object.freeze(Jsuser);   //This is method of freeze objects
+Jsuser.email = "rhtirn@Rupam.com"; 
+console.log(Jsuser.email);
+
+
+Jsuser.greeting = function(){
+    console.log(`Hello ${this.name}`);
+}
+console.log(Jsuser.greeting());
