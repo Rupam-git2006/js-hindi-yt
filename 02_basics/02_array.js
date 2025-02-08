88bbadbb45f9de03fc7a5667215e28c9b4284efe
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-08 03:35:25 +0000

    hi

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -2,3 +2,18 @@ const hero = [" spi","tree", "golem"]
 const dc = ["thor","spider","flash"]
 const all = hero.concat(dc)
 console.log(all);
+
+
+
+const another = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another = another.flat(Infinity)
+console.log(real_another);
+
+console.log(Array.isArray("rupam"));
+console.log(Array.from("rupam"));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
