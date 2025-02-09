a10c56dae6c930e1b889863c6f6a8c08cfb9679e
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-09 14:54:30 +0000

    h

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,11 @@
 //Immediately Invoked Function Expression (IIFE)
+
+//named IIFE
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+})();  // when two IIFE are used, they should be separated by semicolon
+
+( (name) =>{
+    console.log(`DB CONNECTED 2 ${name}`);
+}) ('Chaitanya');
