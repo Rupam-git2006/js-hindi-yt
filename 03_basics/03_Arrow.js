8479e3ec8d41539ae07ef24fd45727ca14bad488
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-09 13:30:29 +0000

    h

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -7,3 +7,30 @@ const user = {
 }
 user.welcomeMsg();
 console.log(this);
+
+
+
+/*
+function chai(){
+    console.log(this);
+    let user = "rupam"
+   // console.log(this.user);  
+}
+chai();*/
+
+
+
+// Arrow function
+const chai = () => {
+    let user = "rupam";
+    console.log(this.user);
+}
+chai(); 
+
+
+
+
+const add = (n1, n2) => (n1 + n2);    //when {} used then return keyword is required & when () is used then return keyword is not required
+console.log(add(10, 20));
+const obj = (n3,n4) => ({username: "rupam"})
+console.log(obj(10,20));
