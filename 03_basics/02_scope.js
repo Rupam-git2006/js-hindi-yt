d92078fe27371c9e74d0e3862bcc42f4b52fb42b
Rupam-git2006 <rupam17samanta@gmail.com>
2025-02-09 07:08:17 +0000

    h

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,5 +1,5 @@
 //In JavaScript, scope is the current context of the code. Scopes can be global or local.var is globaly declared and can be accessed from anywhere in the program.let and const are block scoped and can only be accessed in the block they are declared.
-let b = 10
+/*let b = 10
 const c = 20
 if (true) {
   var a = 1;
@@ -10,5 +10,33 @@ if (true) {
 }
 console.log(a); // 1
 console.log(b); // 2
-console.log(c); // 3
+console.log(c); // 3*/
   
+
+
+
+/*function one(){
+    const username = "John";
+    function two(){
+        const website = "www.google.com";
+        console.log(username);
+    }
+    console.log(website);
+    two()
+}
+one()*/
+
+
+
+
+
+
+
+function addone(num){
+    return num+1;
+}
+addone(5);
+const addtwo = function(num){
+    return num+2;
+}
+console.log(addtwo(5));
